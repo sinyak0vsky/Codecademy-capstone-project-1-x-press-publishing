ff071ba1ae3af2f1b9a724f6486a9c861e3e1cc5
sinyak0vsky <sinyak0vsky@gmail.com>
2020-05-10 19:01:16 +0300

    Add default middleware

--- server.js
+++ server.js
@@ -7,5 +7,11 @@ const errorHandler = require('errorhandler');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware section
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(errorHandler());
+
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
