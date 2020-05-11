71e1ae63ffd95ae168624354129d914c372e15bc
sinyak0vsky <sinyak0vsky@gmail.com>
2020-05-11 12:51:43 +0300

    Add artistId param middleware

--- api/artists.js
+++ api/artists.js
@@ -4,6 +4,20 @@ const sqlite3 = require('sqlite3');
 
 const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite');
 
+// Middleware section
+artistsRouter.param('artistId', (req, res, next, artistId) => {
+  db.get('SELECT * FROM Artist WHERE id = $artistID', {$artistId: artistId}, (err, row) => {
+    if (err) {
+      next(err);
+    }
+    if (!row) {
+      return res.status(404).send();
+    }
+    req.artist = row;
+    next();
+  });
+});
+
 // Get /api/artists
 artistsRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Artist WHERE is_currently_employed = 1', (err, rows) => {
