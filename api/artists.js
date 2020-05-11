049443303c78b35f2b8a92cf324b2ff1edf30a41
sinyak0vsky <sinyak0vsky@gmail.com>
2020-05-11 12:36:23 +0300

    Add Get /api/artists

--- api/artists.js
+++ api/artists.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const artistsRouter = express.Router();
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database(process.env.TEST_DATABASE || '../database.sqlite');
+
+// Get /api/artists
+artistsRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Artist WHERE is_currently_employed = 1', (err, rows) => {
+    if (err) {
+      next(err);
+    }
+    return res.send({ artists: rows});
+  });
+});
 
 module.exports = artistsRouter;
